60e9856a24897560ec5f1dfb8248cb9ae1aebd6f
Vigo28 <gamingvigo06@gmail.com>
2025-06-17 10:04:18 +0300

    after install

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\BookController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/books', [BookController::class, 'index'])->name('book.index');
-Route::get('/books/create', [BookController::class, 'create'])->name('book.create');
-Route::post('/books', [BookController::class, 'store'])->name('book.store');
-Route::get('/books/{book}', [BookController::class, 'show'])->name('book.show');
-Route::get('/books/{book}/edit', [BookController::class, 'edit'])->name('book.edit');
-Route::put('/books/{book}/update', [BookController::class, 'update'])->name('book.update');
-Route::delete('/books/{$book}/destroy', [BookController::class, 'delete'])->name('book.destroy');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
